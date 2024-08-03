42a0c2ae0f3734934ca6b0c355cea0a0653738ea
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2024-08-03 15:38:15 +0900

    モジュールわかんねｗ

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use mouse_rs::{types::keys::Keys, Mouse};
+// mod shiny_mas;
 
 fn main() {
     let mouse = Mouse::new();
@@ -6,4 +7,7 @@ fn main() {
     mouse.move_to(0, 0).expect("move_to failed.");
     mouse.press(&Keys::RIGHT).expect("Press failed.");
     mouse.release(&Keys::RIGHT).expect("Release failed.");
+
+    // ALLタブの順序を取得し、それに合わせて自動でマウスを移動させる
+    // TODO : 曲ごとに何秒待機させるか？
 }
---
+++ src/shiny_mas/song.rs
@@ -0,0 +1,63 @@
+// pub mod shiny_mas {
+//     pub mod song {
+//         /// A song in ShinyColors.
+//         pub struct ShinySong {
+//             String: name,
+//             GroupName: group,
+//             u8: indexOverall,
+//         }
+
+//         impl ShinySong {
+//             pub fn new(name: String, group: GroupName, indexOverall: u8) -> ShinySong {
+//                 ShinySong {
+//                     name,
+//                     group,
+//                     indexOverall,
+//                 }
+//             }
+
+//             pub fn get_all() -> Vec<ShinySong> {
+//                 vec![
+//                     // 全体曲
+//                     ShinySong::new("星の声".to_string(), GroupName::All, 0),
+//                     ShinySong::new("星の声(Another ver.)".to_string(), GroupName::All, 1),
+//                     ShinySong::new("Spread the Wings!!".to_string(), GroupName::All, 2),
+//                     ShinySong::new("Multicolored Sky".to_string(), GroupName::All, 3),
+//                     ShinySong::new("Ambitious Eve".to_string(), GroupName::All, 4),
+//                     ShinySong::new("いつか Shiny Days".to_string(), GroupName::All, 5),
+//                     ShinySong::new("ツバラグラビティ".to_string(), GroupName::All, 6),
+//                     ShinySong::new(
+//                         "サマーサマーオーシャンパーリィバケーション".to_string(),
+//                         GroupName::All,
+//                         7,
+//                     ),
+//                     ShinySong::new(
+//                         "ヒカリのdestination".to_string(),
+//                         GroupName::IlluminationStars,
+//                         8,
+//                     ),
+//                     // イルミネ
+//                     ShinySong::new(
+//                         "We can go now!".to_string(),
+//                         GroupName::IlluminationStars,
+//                         9,
+//                     ),
+//                     ShinySong::new("枕木の歌".to_string(), GroupName::IlluminationStars, 10),
+//                     ShinySong::new("Twinkle way".to_string(), GroupName::IlluminationStars, 11),
+//                 ]
+//             }
+//         }
+//     }
+
+//     pub enum GroupName {
+//         All,
+//         IlluminationStars,
+//         HoukagoClimaxGirls,
+//         Alstroemeria,
+//         Straylight,
+//         Noctile,
+//         Shees,
+//         Cometic,
+//         Solo,
+//     }
+// }
